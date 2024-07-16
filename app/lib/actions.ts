c31aa4630304ee8f52c24c49837a16199c59a879
Jessie S. Zamora <jzamss@gmail.com>
2024-07-16 21:45:21 +0800

    chapter 12: creating invoices using server action

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,20 @@
 "use server";
 
+import { z } from "zod";
+import { sql } from "@vercel/postgres";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(["pending", "paid"]),
+  date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
 export async function createInvoice(formData: FormData) {
   // const rawFormData = {
   //   customerId: formData.get("customerId"),
@@ -7,6 +22,15 @@ export async function createInvoice(formData: FormData) {
   //   status: formData.get("status"),
   // };
   const rawFormData = Object.fromEntries(formData.entries());
-  // Test it out:
-  console.log(rawFormData);
+  const { customerId, amount, status } = CreateInvoice.parse(rawFormData);
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split("T")[0];
+
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES( ${customerId}, ${amountInCents}, ${status}, ${date} )
+  `;
+
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
 }
